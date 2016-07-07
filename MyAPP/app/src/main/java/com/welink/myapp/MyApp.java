9e05e3ece71d574fba8c4486dfa5cb556ece8cef
黄峻眸 <13661398952@163.com>
2016-07-07 18:21:37 +0800

    sdfsd

---
+++ MyAPP/app/src/main/java/com/welink/myapp/MyApp.java
@@ -0,0 +1,17 @@
+package com.welink.myapp;
+
+import android.app.Application;
+
+import org.xutils.x;
+
+/**
+ * Created by dell on 2016/7/7.
+ */
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        x.Ext.init(this);
+    }
+}
